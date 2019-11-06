b44000db6a277538b63230318413669d0e978106
Gabi22top <gabriel_s@hotmail.fr>
2019-11-06 08:25:56 +0100

    Cleaning Dev branch

--- cdr_robot_ws/src/ROS_Libraries/dvb_navigation/src/diff_robot_odometry.cpp
+++
@@ -1,6 +0,0 @@
-#include "dvb_navigation/diff_robot_odometry.h"
-
-void Test::test()
-{
-    
-}
--- cdr_robot_ws/src/ROS_Libraries/dvb_robot/src/robot.cpp
+++
@@ -1,7 +0,0 @@
-#include "dvb_robot/robot.h"
-
-Robot::Robot(bool debug_mode) :
-    debug_mode_(debug_mode)
-{
-    
-}
