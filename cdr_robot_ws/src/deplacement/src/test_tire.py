5270c1540143424bdc340b29b3634f7376ff55e6
Gabriel Soudry <gabriel_s@hotmail.fr>
2020-01-10 13:45:38 +0100

    test_tire.py homologué

--- cdr_robot_ws/src/deplacement/src/test_tire.py
+++ cdr_robot_ws/src/deplacement/src/test_tire.py
@@ -13,6 +13,7 @@ class Robot():
       self.pub = rospy.Publisher('/speedLeft', Int16, queue_size=10)
       self.pub2 = rospy.Publisher('/speedRight', Int16, queue_size=10)
       self.go = 1
+      
       self.tirette = rospy.Subscriber ("/PinGo", Int16, self.tirette_callback)
       self.ultrasons = rospy.Subscriber("/ultrasound", Range, self.ultrasound_cb)
 
@@ -38,32 +39,36 @@ class Robot():
    
    def ultrasound_cb(self, msg):
       self.distance = msg.range
-      rospy.loginfo("La distance est %i",self.distance)
-
-
+      #rospy.loginfo("La distance est %i",self.distance)
+   
+   def straight(self, time):
+      begin = rospy.get_rostime()
+      now = rospy.get_rostime()
+      temps_ecoule = 0
+      while (now.secs - begin.secs < time):
+         if (self.distance < 40):
+            self.stop()
+            temps_ecoule = rospy.get_time() - now.secs
+            rospy.loginfo("Le temps d'attente est de %i",temps_ecoule)
+            continue
+         self.pub.publish(50)
+         self.pub2.publish(50)
+         begin.secs = temps_ecoule + begin.secs
+         now = rospy.get_rostime()
+         temps_ecoule = 0
+      rospy.loginfo("Fin de la ligne droite")
+      self.stop()
+      rospy.spin()
 
    def run(self):
-	   while (self.go == 1):
-	      rospy.loginfo("J'att")
-	      self.pub.publish(0)       
-         self.pub2.publish(0)
-
-      begin = rospy.get_rostime()
-	   rospy.loginfo("Current Time is %i %i",begin.secs,begin.nsecs)
-	   now = rospy.get_rostime()
-	   self.pub.publish(10)
-	   self.pub2.publish(10)   
-	   while (now.secs - begin.secs < 2.5) :
-            if (self.distance < 40):
-               a = 1
-            self.pub.publish(50)	   
-            self.pub2.publish(50)
-            now = rospy.get_rostime() 
-	
-	self.pub.publish(0)       
-        self.pub2.publish(0)
-	
-	rospy.spin()
+      while(self.go == 1):
+         rospy.loginfo("J'att")
+         self.stop()
+      
+      #self.pub.publish(10)
+      #self.pub2.publish(10)
+      self.straight(2)
+               
 	   	
 
 if __name__ == '__main__':
