10ce2040731596f88a28a1b16ca63f9b29ffb6bd
leo <leo@leoasus.home>
2020-04-09 13:00:41 +0200

    Differents PID added

---
+++ Firmware/PID_WheelLeft/src/PidLeft_RobotLesb.cpp
@@ -0,0 +1,110 @@
+/*
+
+#define ENCODER_OPTIMIZE_INTERRUPTS
+#include <Encoder.h>
+#include <PID_v1.h>
+#include <ros.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Float64.h>
+
+int EnA = 2;
+int In1 = 3; 
+int In2 = 4; 
+
+int count = 0;
+double sens = 1;
+
+Encoder knobLeft(5, 6);
+double kp = 2, ki = 0.7, kd = 0.05;            
+
+ros::NodeHandle nh;
+
+double input2, output2, setpoint2; 
+PID myPID2(&input2, &output2, &setpoint2, kp, ki, kd, DIRECT); 
+
+
+void messageCb( const std_msgs::Int16& toggle_msg){
+  setpoint2=toggle_msg.data;
+  if (setpoint2 < 0){
+    digitalWrite(In1, HIGH);
+    digitalWrite(In2, LOW);
+    sens = 1;
+    setpoint2 = -setpoint2;
+
+  }
+  else if (setpoint2 > 0){
+    digitalWrite(In1, LOW);
+    digitalWrite(In2, HIGH);
+    sens = -1;
+  }
+}
+std_msgs::Float64 enco_left;
+ros::Publisher encoder_left("encoLeft", &enco_left);
+
+ros::Subscriber<std_msgs::Int16> sub("/speedLeft", &messageCb );
+
+void setup() {
+
+  nh.initNode();
+  nh.subscribe(sub);
+  nh.advertise(encoder_left);
+
+  pinMode(EnA, OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+
+  input2 = 0;
+  setpoint2 = 0;
+
+  myPID2.SetMode(AUTOMATIC);
+  myPID2.SetSampleTime(10); //Fréquence du PID dans le loop
+  myPID2.SetOutputLimits(0, 200); //Va fixer le PWM entre -400 et 400 comme sur nos moteurs
+
+  //Serial.begin (115200);                              //DEBUG
+}
+
+void asservissement(double cible, bool arret)
+{
+  setpoint2=cible;
+  input2 = knobLeft.read() * sens;
+
+  if (myPID2.Compute()) {
+    count++;
+    /*
+    
+    Serial.print(input2);
+    Serial.print(" , ");
+    Serial.print(" , ");
+    Serial.print(setpoint2);
+    Serial.println();
+    
+
+   if (output2 >= 0 && setpoint2!=0) {
+      analogWrite(EnA, output2);
+    }
+    else if (setpoint2==0) {
+      analogWrite(EnA,0);
+    }
+    enco_left.data= input2;
+    knobLeft.write(0);
+  }
+}
+
+void loop()
+{ 
+  nh.spinOnce();
+  delay(1);
+  asservissement(setpoint2, false);
+  
+  if (count == 1){
+    
+    encoder_left.publish(&enco_left);
+    count = 0;
+  }
+  
+  //encoder_left.publish(&enco_left);
+}
+*/
---
+++ Firmware/PID_WheelRight/src/CodeRight.cpp
@@ -0,0 +1,95 @@
+/*+------------/*
+ * Code arduino pour codeurs incrémentaux magnétique ou optique.
+ * 
+ * Utilisation avec codeurs Kuebler 2400 mini
+ * https://www.kuebler.com/k2014/j/fr/produkte/details/drehgeber/Inkremental//Miniatur/2400
+ * ref : 05.2400.1122.1024
+ * check branchements sur la datasheet en fonction de la ref
+ * 
+ * Codeur incrémental avec push-pull : 6 signaux délivrés
+ * On n'utilise que 2 signaux ( A & B ) pour la 1ère version
+ * 
+ * Branchement codeur sans push-pull :
+ *    - BROWN : 5V
+ *    - WHITE : GND
+ *    - GRAY : SIGNAL B en PIN 7 pour gauche, PIN 8 pour droit
+ *    - GREEN : SIGNAL A en PIN 2 pour gauche, PIN 3 pour droit
+ *    
+ * 
+*/
+
+/************* SETUP ******************/
+
+#define EnA 2
+#define In1 3
+#define In2 4
+#define TOPIC_ENCODER_NAME "/robot/base/wheel/right/state"
+#define TOPIC_ENCODER_RESET_NAME "/robot/base/wheel/right/reset"
+#define TOPIC_MOTOR_RIGHT_NAME "/robot/base/wheel/right/control_effort"
+
+
+/**************************************/
+
+#include <ros.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Int16.h>
+#include <Encoder.h>
+
+ros::NodeHandle nh;
+
+volatile int pos;
+int interruptA;
+std_msgs::Float64 value_encodeur;
+double value_PID_Rasp;
+
+Encoder knobLeft(5, 6);
+std_msgs::Float64 encoder_pos;
+/*
+void encoder_reset(const std_msgs::Int16& toggle_msg) {
+  pos=0;
+}
+*/
+void messageCb( const std_msgs::Float64& msg){
+  value_PID_Rasp = msg.data;
+  if (value_PID_Rasp <= 0){
+    digitalWrite(In1,HIGH);
+    digitalWrite(In2,LOW);
+    analogWrite(EnA,-value_PID_Rasp);
+  }
+  else{
+    digitalWrite(In1,LOW);
+    digitalWrite(In2,HIGH);
+    analogWrite(EnA,value_PID_Rasp);
+  }
+
+}
+
+ros::Publisher pub_encoder(TOPIC_ENCODER_NAME, &encoder_pos);
+/*
+ros::Subscriber <std_msgs::Int16>  sub_encoder(TOPIC_ENCODER_RESET_NAME, &encoder_reset);
+*/
+ros::Subscriber <std_msgs::Float64> sub_tension(TOPIC_MOTOR_RIGHT_NAME, &messageCb);
+
+
+
+void setup()
+{
+   nh.initNode();
+   nh.advertise(pub_encoder);
+   
+   nh.subscribe(sub_tension);
+   pos = 0;
+   
+   pinMode(EnA, OUTPUT);
+   pinMode(In1, OUTPUT);
+   pinMode(In2, OUTPUT);
+   digitalWrite(In1,LOW);
+   digitalWrite(In2,HIGH);
+}
+ 
+void loop(){
+  nh.spinOnce();
+  value_encodeur.data = knobLeft.read();
+  pub_encoder.publish( &value_encodeur);
+  delay(10);
+}
