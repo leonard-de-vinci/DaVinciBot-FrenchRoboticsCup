27c4f1d2d6037ccb3f2c9846f73d669e1dabf98e
COLVERTYETY <niconoob1@gmail.com>
2020-11-26 21:17:32 +0100

    added complete

---
+++ Firmware/N2complete/include/config.h
@@ -0,0 +1,19 @@
+// topic names
+
+#define TOPIC_TARGET "/N1/target"
+#define TOPIC_REALITY "/N1/reality"
+#define PARAM_PID "lpid"
+#define TOPIC_UPDATEPID "/npid"
+//pins
+#define pin_encoder 6// pin for the encoder interrupt
+#define pin_encoder2 5// pin for the encoder interrupt
+#define pin_dir1 4//pin for H bridge pwm
+#define pin_dir2 3//pin for H bridge pwm
+#define pin_pwr   2 //pin for H bridge enable
+
+volatile int kp = 30;
+volatile int ki = 30;
+//volatile int kd = 0;
+
+const int IMAX = 2046;
+const int IMIN = -2046;
---
+++ Firmware/N2complete/src/main.cpp
@@ -0,0 +1,130 @@
+#include "TimerOne.h"
+#include <Arduino.h>
+#include "header.h"
+
+void setup(void)
+{
+  //ROS
+  nh.initNode();
+  //initialisation du Node ROS
+  nh.advertise(pub_reality);         //advertise le topic de publication
+  nh.subscribe(sub_target);
+  //nh.subscribe(sub_empty);          //on s'abonne à là où on va écouter
+  nh.subscribe(sub_emergency_break); //abonnement arrêt d'urgence
+  while (!nh.connected())
+  {
+    nh.spinOnce();
+  }
+  //TIMER initialization
+  Timer1.initialize(period);     //initialisation du timer
+  Timer1.attachInterrupt(Cycle); //attachInterrupt
+  //pin init
+  pinMode(pin_encoder2,INPUT);
+  pinMode(pin_pwr, OUTPUT);
+  digitalWrite(pin_pwr,LOW);
+  pinMode(pin_dir1, OUTPUT);
+  digitalWrite(pin_dir1,LOW);
+  pinMode(pin_dir2, OUTPUT);
+  digitalWrite(pin_dir2,LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(pin_encoder, INPUT);
+  //encoder initialisation
+  attachInterrupt(digitalPinToInterrupt(pin_encoder), encoderInterrupt, RISING); //! slow must be changed to attachInterruptVector
+  //attachInterruptVector(,encoderInterrupt,RISING);
+  /*uint32_t mask = (0x09 << 16) | 0x01000000;//setup mask for rising edge
+  volatile uint32_t *config;
+  config = portConfigRegister(pin_encoder);
+  */
+  //setting up pwm precision
+  analogWriteFrequency(pin_pwr, 10000); //setting up ideal frequency pedending on cpu frequency
+  analogWriteResolution(10);                  // 0 - 255
+}
+
+void loop(void) ///main loop
+{
+  nh.spinOnce();
+  if (mainlooppub)
+  {
+    // create new message
+    reality_pub.ticks = reality_ticks; //reality_ticks;
+    reality_pub.cycles = target_cycles;
+    //publish new message
+    pub_reality.publish(&reality_pub);
+    mainlooppub = false;
+  }
+  digitalWrite(LED_BUILTIN, emergency_break);
+}
+
+void Cycle() ///called by the timer
+{
+  cli(); //éteint les interrupts
+  if (emergency_break || (target_cycles<=0))
+  {
+    motorbreak();
+  } else 
+  {
+    //calculate error and pid
+    e = target_ticks - tick;
+    E = E + e;
+    //de = e - olde;
+    PID_ = (kp * e); 
+    int temp = (ki * E);// + (kd * de);
+    int I = (temp < IMAX) ? temp : IMAX;
+    I = (I < IMIN) ? IMIN : I;
+    PID_ +=I;
+    mapped = ( PID_ < 0) ? 0 : PID_;
+    mapped = ( PID_ < 1023) ? PID_ : 1023;
+    analogWrite(pin_pwr, mapped);
+    //reset
+    //olde = e;
+    
+    target_cycles--;
+  }
+  reality_ticks = tick;
+  tick = 0;
+  mainlooppub = true;
+  sei(); //relance les interrupts
+}
+
+void encoderInterrupt()
+{
+  if(digitalReadFast(pin_encoder2)){
+    tick++;
+  }else{
+    tick--;
+  }
+}
+void motorbreak()
+{
+  //! must be tested
+  digitalWrite(pin_pwr, LOW);
+  digitalWrite(pin_dir1, HIGH);
+  digitalWrite(pin_dir2, HIGH);
+  //digitalWrite(pin_pwr,LOW);
+  
+}
+void emergency_break_callback(const std_msgs::Bool &msg)
+{
+  emergency_break = msg.data;
+  if (emergency_break)
+  {
+    motorbreak();
+  }
+  else
+  {
+    digitalWrite(pin_dir1, dir);
+    digitalWrite(pin_dir2, !dir);
+  }
+}
+void target_callback(const PID::IntArr &msg)
+{
+  target_ticks = msg.ticks;
+  target_cycles = msg.cycles;
+  dir = (target_ticks > 0);
+  E = 0;
+  if(! emergency_break){
+    digitalWrite(pin_dir1, dir);
+    digitalWrite(pin_dir2, !dir);
+  }
+  
+}
