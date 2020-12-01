e692be3f5ddf4e1f5e0e6308e62dbe7d98babf3c
Antoine Raulin <antoin.raulin@gmail.com>
2020-12-01 16:54:14 +0100

    liteComplete + setupIP.sh

---
+++ Firmware/liteComplete/lib/PID/ros_lib/PID/FloatArr.h
@@ -0,0 +1,86 @@
+#ifndef _ROS_PID_FloatArr_h
+#define _ROS_PID_FloatArr_h
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ros/msg.h"
+
+namespace PID
+{
+
+  class FloatArr : public ros::Msg
+  {
+    public:
+      typedef float _v_type;
+      _v_type v;
+      typedef float _theta_type;
+      _theta_type theta;
+
+    FloatArr():
+      v(0),
+      theta(0)
+    {
+    }
+
+    virtual int serialize(unsigned char *outbuffer) const override
+    {
+      int offset = 0;
+      union {
+        float real;
+        uint32_t base;
+      } u_v;
+      u_v.real = this->v;
+      *(outbuffer + offset + 0) = (u_v.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_v.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_v.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_v.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->v);
+      union {
+        float real;
+        uint32_t base;
+      } u_theta;
+      u_theta.real = this->theta;
+      *(outbuffer + offset + 0) = (u_theta.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_theta.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_theta.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_theta.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->theta);
+      return offset;
+    }
+
+    virtual int deserialize(unsigned char *inbuffer) override
+    {
+      int offset = 0;
+      union {
+        float real;
+        uint32_t base;
+      } u_v;
+      u_v.base = 0;
+      u_v.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_v.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_v.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_v.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->v = u_v.real;
+      offset += sizeof(this->v);
+      union {
+        float real;
+        uint32_t base;
+      } u_theta;
+      u_theta.base = 0;
+      u_theta.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_theta.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_theta.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_theta.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->theta = u_theta.real;
+      offset += sizeof(this->theta);
+     return offset;
+    }
+
+    virtual const char * getType() override { return "PID/FloatArr"; };
+    virtual const char * getMD5() override { return "a5efdc7608f256f976ad7573b0f4e032"; };
+
+  };
+
+}
+#endif
