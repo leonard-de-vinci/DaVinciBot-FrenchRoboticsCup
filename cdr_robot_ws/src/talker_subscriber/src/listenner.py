5a2a4e66d3208eb5c505dd85ec3cb24ce09441b9
Antoine Raulin <antoin.raulin@gmail.com>
2020-12-11 11:58:37 +0100

    changes

--- cdr_robot_ws/src/talker_subscriber/src/listenner.py
+++ cdr_robot_ws/src/talker_subscriber/src/listenner.py
@@ -15,7 +15,7 @@ def listener():
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("chatter", String, callback)
-
+    print("starting")
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
