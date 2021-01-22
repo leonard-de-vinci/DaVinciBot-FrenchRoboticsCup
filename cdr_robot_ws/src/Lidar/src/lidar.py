e8adc5c7389d6cd734149fbec446191b9c9bf4f3
sn00wden <matthieu2603@gmail.com>
2021-01-22 12:43:36 +0100

    Try Lidar

--- cdr_robot_ws/src/Lidar/src/lidar.py
+++ cdr_robot_ws/src/Lidar/src/lidar.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 
 import rospy
+import numpy as np
 #from PID.msg import IntArr
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
 
 
 def Lidar_setup():
-    lidar_publish = rospy.Publisher('Lidar_validation',Bool,queue_size=1)
-    rospy.Subscriber('/scan',LaserScan,Lidar_usings)
+    
     rospy.init_node('Lidar')
-    rospy.loginfo("> coucou")
+    global lidar_publish = rospy.Publisher('Lidar_validation',Bool,queue_size=1)
+    sub = rospy.Subscriber('/scan',LaserScan,Lidar_usings)
+    
+    rospy.loginfo("Le node Lidar a démarré correctement")
     
    
 
 
 def Lidar_usings(laser_scan):
     #angle_increment = laser_scan.angle_increment
-    print(laser_scan)
-    if laser_scan.range_min <= 0.01 :
-        lidar_publish.publish(False)
+    ranges = np.array(laser_scan.range)
+    test = True
+    for i in range(len(ranges)):
+        if ranges[i] < 0.10 :
+            test = False
+            lidar_publish.publish(test)
+
 
 
 if __name__ == '__main__':
+    
     Lidar_setup()
+    rospy.spin()
 
 
