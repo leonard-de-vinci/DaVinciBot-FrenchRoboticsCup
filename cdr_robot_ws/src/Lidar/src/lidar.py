36789abb858299a862a995b8a057a1d3361b4611
sn00wden <matthieu2603@gmail.com>
2021-01-22 14:20:13 +0000

    Ajout lidar.py

--- cdr_robot_ws/src/Lidar/src/lidar.py
+++ cdr_robot_ws/src/Lidar/src/lidar.py
@@ -8,26 +8,50 @@ from std_msgs.msg import Bool
 
 
 def Lidar_setup():
-    
+    global lidar_publish, buffer
+    buffer = 0
     rospy.init_node('Lidar')
-    global lidar_publish = rospy.Publisher('Lidar_validation',Bool,queue_size=1)
+    lidar_publish = rospy.Publisher("breakServo",Bool,queue_size=1)
     sub = rospy.Subscriber('/scan',LaserScan,Lidar_usings)
-    
-    rospy.loginfo("Le node Lidar a démarré correctement")
-    
-   
+    rospy.loginfo("Le node Lidar a demarre correctement")
 
 
 def Lidar_usings(laser_scan):
     #angle_increment = laser_scan.angle_increment
-    ranges = np.array(laser_scan.range)
-    test = True
+    global buffer, lidar_publish
+    ranges = np.array(laser_scan.ranges)
+    test = False
     for i in range(len(ranges)):
-        if ranges[i] < 0.10 :
-            test = False
-            lidar_publish.publish(test)
-
-
+        if ranges[i] < 1.00 and ranges[i]>0.06 :
+            test = True
+            rospy.loginfo(str(ranges[i]))
+    if test == True :
+    	buffer+= 1
+    else :
+    	buffer -= 1
+    if buffer <0:
+    	buffer = 0
+    if buffer > 10 :
+    	msg = Bool()
+    	msg.data = True
+    	lidar_publish.publish(msg)
+        #rospy.loginfo("breaking!!")
+    else :
+    	msg = Bool()
+    	msg.data = False
+    	lidar_publish.publish(msg)
+    if buffer >20 :
+    	buffer = 20
+    rospy.loginfo(str(test))
+    #rospy.loginfo(str(laser_scan.range_min))
+    #if laser_scan.range_min <1.0 :
+    #	msg = Bool()
+    #	msg.data = True
+    #	lidar_publish.publish(msg)
+    #else:
+    #	msg = Bool()
+    #	msg.data = False
+    #	lidar_publish.publish(msg)
 
 if __name__ == '__main__':
     
