9e6c7363c3e5a448ee8f440f2875a083fe580b95
COLVERTYETY <niconoob1@gmail.com>
2021-02-05 10:19:48 +0100

    changed the stuff

--- cdr_robot_ws/src/brain/src/app.py
+++ cdr_robot_ws/src/brain/src/app.py
@@ -58,7 +58,7 @@ def mainloop():
         if actionpos >= len(waypoints):
             state += 1
         else:
-            rospy.loginfo((str)(actionpos))
+            rospy.loginfo("action n°"+(str)(actionpos))
             currentaction = waypoints[actionpos]
             senderid = currentaction[0]
             # ##------------------- what are we waiting for
